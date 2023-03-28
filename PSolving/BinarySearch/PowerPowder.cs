6823176cc6890f6926ab6eb76ceaca73195c1648
Amr Elshaer <amrelsher07@gmail.com>
2023-03-28 23:32:41 +0200

    add power of powder problem that using binary search tech

---
+++ PSolving/BinarySearch/PowerPowder.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSolving.BinarySearch
+{
+    internal class PowerPowder
+    {
+        bool CanPick(int cookies, int[] needed, int[] got, int k, int n)
+        {
+            int md = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (got[i] >= needed[i] * cookies) continue;
+                md += needed[i] * cookies - got[i];
+                if (md > k) return false;
+            }
+            return md <= k;
+        }
+
+        int BinarySearch(int[] needed, int[] got, int n, int k)
+        {
+            int s = 0;
+            int en = (int)Math.Pow(10, 9);
+
+            while (s < en)
+            {
+                int mid = s + (en - 1) / 2;
+                // validate with binary seaerch all before is accepted all after is not accepted
+                if (CanPick(mid, needed, got, k, n))
+                {
+                    s = mid;
+                }
+                else
+                {
+                    en = mid - 1;
+                }
+            }
+            return s;
+        }
+
+        void Run()
+        {
+            var fsLine = Console.ReadLine();
+            var fs = fsLine.Split(' ').Select(int.Parse).ToArray();
+            var scLine = Console.ReadLine();
+            var needed = scLine.Split(' ').Select(int.Parse).ToArray();
+            var tcLine = Console.ReadLine();
+            var got = tcLine.Split(' ').Select(int.Parse).ToArray();
+            var n = fs[0];
+            var k = fs[1];
+            Console.WriteLine(BinarySearch(needed, got, n, k));
+        }
+    }
+}
--- PSolving/Program.cs
+++ PSolving/Program.cs
@@ -1,41 +1,4 @@
-﻿using PSolving;
-using PSolving.SlidingWindowProblems;
-using System.Reflection;
-using System.Threading.Channels;
+﻿
 
-var digits=new int[] { 1, 2, 3, 5, 8, 13, 21, 34 };
-int[] res = new int[8];
-Solve(0,0);
-void Solve(int pos,int cn)
-{
-    if (pos==digits.Length)
-    {
-        if (cn==6)
-        {
-            for (int i = 0; i < 6; i++)
-            {
-                Console.Write(res[i]);
-            }
-            Console.WriteLine(); 
-        }
-    }
-    else
-    {
-        res[cn] = digits[pos];
-        // pick
-       Solve(pos+1, cn+1);
-       // leave
-       Solve(pos+1, cn);
-    }
-}
+Run();
 
-
-//var instances = Assembly.GetExecutingAssembly().GetTypes()
-//    .Where(t => t.GetInterfaces().Contains(typeof(IExecutableCode))
-//                && t.GetConstructor(new Type[0]) != null)
-//    .Select(t => (IExecutableCode)Activator.CreateInstance(t));
-
-//foreach (var instance in instances)
-//{
-//    instance.Execute();
-//}
